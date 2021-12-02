d57e1a501f98ce1c7531e88ac2916d74a8dafd6a
Mike Willbanks <me@mike.engineer>
2021-12-02 09:19:33 -0600

    fix: cleanup formatting

--- 2_dive/run.js
+++ 2_dive/run.js
@@ -1,38 +1,41 @@
-const fs = require('fs');
-const input = fs.readFileSync('./input.txt', { encoding: 'UTF8' }).trim().split("\n");
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt", { encoding: "UTF8" })
+  .trim()
+  .split("\n");
 
 let depth = 0;
 let hzPos = 0;
-let aim   = 0;
+let aim = 0;
 
 // part 1
-input.map(i => {
+input.map((i) => {
   const n = +i.substr(i.indexOf(" ") + 1);
-  if (i[0] === 'f') {
+  if (i[0] === "f") {
     hzPos += n;
   } else {
-    depth += (i[0] === 'd') ? n : n * -1;
+    depth += i[0] === "d" ? n : n * -1;
   }
 });
 
-console.log('Horizontal Position', hzPos);
-console.log('Depth', depth);
-console.log('Total', hzPos * depth);
+console.log("Horizontal Position", hzPos);
+console.log("Depth", depth);
+console.log("Total", hzPos * depth);
 
 // part 2:
 depth = 0;
 hzPos = 0;
-aim   = 0;
-input.map(i => {
+aim = 0;
+input.map((i) => {
   const n = +i.substr(i.indexOf(" ") + 1);
-  if (i[0] === 'f') {
+  if (i[0] === "f") {
     hzPos += n;
     depth += aim * n;
   } else {
-    aim += (i[0] === 'd') ? n : n * -1;
+    aim += i[0] === "d" ? n : n * -1;
   }
 });
 
-console.log('Horizontal Position', hzPos);
-console.log('Depth', depth);
-console.log('Total', hzPos * depth);
+console.log("Horizontal Position", hzPos);
+console.log("Depth", depth);
+console.log("Total", hzPos * depth);
